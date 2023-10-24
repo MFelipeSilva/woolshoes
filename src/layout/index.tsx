a80385db0981e56fa79d709a4ff0d15a23949687
Felipe da Silva <silvaafelipe016@gmail.com>
2023-10-24 15:01:19 -0300

    fix: adjusted import

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import GlobalStyle from "@/styles/global";
 
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 import { Container } from "./styles";
-import { Footer } from "@/components/Footer";
 
 export const Layout = ({ children }: any) => {
   return (
