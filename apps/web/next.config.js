2fe896a2e99f156520156459b904c65a58824e9d
Felipe da Silva <silvaafelipe016@gmail.com>
2023-10-31 19:14:58 -0300

    feat: added turbo monorepo and some changes

---
+++ apps/web/next.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
+function getCurrentDirname(importMetaUrl) {
+  const __filename = fileURLToPath(importMetaUrl);
+  return path.dirname(__filename);
+}
+
+export default {
+  webpack: (config) => {
+    const currentDir = getCurrentDirname(import.meta.url);
+
+    config.resolve.alias["@"] = currentDir;
+
+    return config;
+  },
+};
