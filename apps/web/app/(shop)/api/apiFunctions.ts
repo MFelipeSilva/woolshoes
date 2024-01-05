ab1057b84afda42a9c80398ea1f6e95a0f1f494a
Felipe da Silva <silvaafelipe016@gmail.com>
2024-01-05 15:28:43 -0300

    feat: small change backend

--- apps/web/app/(shop)/api/apiFunctions.ts
+++ apps/web/app/(shop)/api/apiFunctions.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 export const getProduct = async (slug: string) => {
-  const { data } = await axios.get(`http://localhost:3001/product/${slug}`);
+  const { data } = await axios.get(
+    `https://woolshoes-api.onrender.com/product/${slug}`
+  );
   return data;
 };
 
 export const getCategory = async (slug: string) => {
-  const { data } = await axios.get(`http://localhost:3001/category/${slug}`);
+  const { data } = await axios.get(
+    `https://woolshoes-api.onrender.com/category/${slug}`
+  );
   return data;
 };
